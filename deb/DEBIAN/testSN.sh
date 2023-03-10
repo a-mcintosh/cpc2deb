abe0186b2ea8dbfbf15adc5b703fad2a38c8df39
Aubrey McIntosh, PhD <aubrey.mcintosh@utexas.edu>
2023-03-10 07:41:57 -0600

    reworked filter.testSN, haven't tested

--- deb/DEBIAN/testSN.sh
+++ deb/DEBIAN/testSN.sh
@@ -6,5 +6,5 @@ cat > /tmp/${gitID8}/source
 cat < /tmp/${gitID8}/source | grep Version >/tmp/${gitID8}/ver
 oldV=`cat < /tmp/${gitID8}/ver` 
 newV=`echo $oldV | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}'`
-sed "s/$oldV/$newV/g" < /tmp/${gitID8}/source
+sed "s/$oldV/$newV/g" < /tmp/${gitID8}/source | testSmudge.sh
 
